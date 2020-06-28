e13177294e1ed25f26d07ee6ffc601ea534b4f20
Elizaveta <krasnova@proitr.ru>
2020-06-28 18:57:02 +0500

    push

---
+++ service.js
@@ -0,0 +1,28 @@
+'use strict';
+const express = require('express');
+const webpush = require('web-push');
+
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+
+webpush.setVapidDetails('mailto:krasnovaliza575@mail.com', publicVapidKey, privateVapidKey);
+
+const app = express();
+
+app.use(require('body-parser').json());
+
+app.post('/subscribe', (req, res) => {
+    const subscription = req.body;
+    res.status(201).json({});
+    const payload = JSON.stringify({ title: 'test' });
+
+    console.log(subscription);
+
+    webpush.sendNotification(subscription, payload).catch(error => {
+        console.error(error.stack);
+    });
+});
+
+app.use(require('express-static')('./'));
+
+app.listen(3000);
