f77a71f159b3195131b6e68cd1af48270f43ea0c
Elizaveta <krasnova@proitr.ru>
2020-07-02 12:29:05 +0500

    push

--- service.js
+++ service.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const webpush = require('web-push');
 
-const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
-const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+const publicVapidKey = 'BBXVirIB08nGFB_KwhHkTlS_hOqNY7CkfcdrI_kGpPdzPOxB45KLvd5PaM_ISdguCsKuwPfIc12XDjS_NrhtaW4' || process.env.PUBLIC_VAPID_KEY;
+const privateVapidKey = '9Q0eYfwaS5j2dSb8o02xNZrMxeKoj73lEG6RQmhnclw' || process.env.PRIVATE_VAPID_KEY;
 
 webpush.setVapidDetails('mailto:krasnovaliza575@mail.com', publicVapidKey, privateVapidKey);
 
