956ffa77e65d4cd85d5be2c2a297f222e9fc6ad3
Elizaveta <krasnova@proitr.ru>
2020-07-02 12:41:03 +0500

    push

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ console.log('Loaded service worker!');
 self.addEventListener('push', ev => {
     const data = ev.data.json();
     console.log('Got push', data);
-    console.log(self.registration)
+    console.log(self.registration);
     self.registration.showNotification(data.title, {
         body: 'Hello, World!',
     });
